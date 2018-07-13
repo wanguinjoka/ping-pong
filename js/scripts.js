02d5bd8bf8f9ee4317c9a3a451d9c0faa4499df8
wangui.njoka <wangui.njoka@gmail.com>
2018-07-13 13:32:49 +0300

    entered function to cover 3 and number divisible by 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,13 @@ $("form#enterNum").submit (function(event){
 	event.preventDefault();
   var userInput=parseInt($("input#number").val());
 
-  console.log(userInput)
-
-  $(".series").text(userInput);
-
+  for(var i=1; i<= userInput; i++)
+    if(i){
+      var output ="";
+      if (i%3!=0 && i%5!=0) output=(i)
+      if(i%3==0)output ="Ping";
+      if(i%5==0)output +="Pong";
+      $(".series").append(output+"</br>");
+    }
   }) ;
-
   })
