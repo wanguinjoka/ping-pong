36bde7bd722edcfa429b8a5a3afb869c1a5012a7
wangui.njoka <wangui.njoka@gmail.com>
2018-07-13 12:34:49 +0300

    added form to the main section

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+$("form#enterNum").submit (function(event){
+	event.preventDefault();
+  var userInput=parseInt($("input#number").val());
 
+  console.log(userInput)
+
+  $(".series").text(userInput);
+
+  }) ;
+
+  })
